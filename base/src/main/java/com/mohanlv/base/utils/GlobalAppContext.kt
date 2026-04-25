9f9bff4fb5070a16c48d67945ef129eb847404fa
lvtong199881 <you@example.com>
2026-04-26 05:13:59 +0800

    chore：workflow test，change base module2

--- base/src/main/java/com/mohanlv/base/utils/GlobalAppContext.kt
+++ base/src/main/java/com/mohanlv/base/utils/GlobalAppContext.kt
@@ -7,12 +7,12 @@ import android.app.Application
  * 在 Application.onCreate() 中初始化
  */
 object GlobalAppContext {
-    
+
     private var application: Application? = null
-    
+
     fun init(application: Application) {
         this.application = application
     }
-    
+
     fun get(): Application? = application
 }
